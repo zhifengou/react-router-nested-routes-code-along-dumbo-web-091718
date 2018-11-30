7c84b2beef42621d784b6cfef7347bdf531dc768
zhifeng ou <zhifengou@hotmail.com>
2018-11-30 02:10:29 +0000

    Done.

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -7,6 +7,10 @@ import MovieShow from './MovieShow';
 const MoviesPage = ({ match, movies }) => (
   <div>
     <MoviesList movies={movies} />
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a Movie from the list.</h3>
+    )}/>
+    <Route path={`${match.url}/:movieId`} component={MovieShow}/>
   </div>;
 
 )
